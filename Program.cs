5a831264d64d930c59ca0fc25aaa1caa5e32d5d9
darsh1206 <darshpatelahm@gmail.com>
2024-02-24 21:31:48 -0500

    added functions to mange the arguments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,107 @@
-﻿using Client;
+﻿/*
+ * Project: Assignment 3
+ * File: Program.cs
+ * Author: Darsh Patel(8870657) and Bhumitkumar Patel(8847159)
+ * Description: This file contains the main for the test client from where the auto or manual test starts
+ * Date: 2024-02-24
+ */
+using Client;
 using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 
 namespace Client
 {
     public class Program
     {
-        
+        /*
+         * Function: parseArguments()
+         * Parameters: string[] args: command line arguments
+         *             ref string serverAddress: server IP address which needs to be extracted from command line arguments
+         *             ref int portNo: port number of server which needs to be extracted from command line arguments
+         *             ref bool isAutoMode: true if auto mode
+         *             ref bool isManualMode: true if manual mode
+         * Description: This function parse the given command line arguments into specific variables and flags.
+         * Return values: bool: true if single flag on or false
+         */
+        private static bool parseArguments(string[] args, ref string serverAddress, ref int portNo, ref bool isAutoMode, ref bool isManualMode)
+        {
+            // Check the number of command line arguments
+            if (args.Length < 2)
+            {
+                return false;
+            }
+            // extract server address and port number
+            serverAddress = args[0];
+            if (!int.TryParse(args[1], out portNo))
+            {
+                return false;
+            }
+
+            // Check for optional parameters
+            for (int i = 2; i < args.Length; i++)
+            {
+                if (args[i] == "-auto")
+                {
+                    isAutoMode = true;
+                }
+                else if (args[i] == "-manual")
+                {
+                    isManualMode = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            // Ensure only one mode is specified
+            if (isAutoMode == isManualMode)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /*
+         * Function: initiateOperation()
+         * Parameters: Uri serverUri: server URL
+         *             bool manual: manual flag
+         * Description: This function initiates the manual/auto operation based on the flag
+         * Return values: void
+         */
+        private static async Task initiateOperation(Uri serverUri, bool manual)
+        {
+
+           
+        }
+
+        static async Task Main(string[] args)
+        {
+            // variables
+            string serverAddress = "";
+            int portNo = 0;
+            bool auto = false;
+            bool manual = false;
+
+            // parsed command line arguments
+            if (!parseArguments(args, ref serverAddress, ref portNo, ref auto, ref manual))
+            {
+                Console.WriteLine("Usage: Program.exe <IP Address> <Port No.> <-auto | -manual>");
+                Console.WriteLine("<IP Address>: Server's IP Address");
+                Console.WriteLine("<Port No.>: Server's Port Number");
+                Console.WriteLine("<-auto | -manual>: Flags for test operation (only one at a time)");
+                return;
+            }
+            // created a url to connect to server
+            Uri serverUri = new Uri($"ws://{serverAddress}:{portNo}");
+
+            await initiateOperation(serverUri, manual);
+        }
     }
 }
